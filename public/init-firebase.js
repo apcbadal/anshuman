1aa7fac624fda1f424d99aa7ed3d05503a85f52e
Anshuman Badal <apcbadal@gmail.com>
2020-07-31 02:19:54 +0530

    Implementing File upload on firebase

---
+++ public/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAmSq2vvs6k8nuwJuG3mvFkUiGK9olysB4  ",
+    authDomain: "slackanshu.firebaseapp.com",
+    databaseURL: "https://file-upload-deae7.firebaseio.com",
+    projectId: "slackanshu",
+    storageBucket: "slackanshu.appspot.com",
+    messagingSenderId: "146421699965",
+    
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
